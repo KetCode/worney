f3c2084b1f3c8245056abd5685d999cb035630f4
Kesse <kesse.matias@hotmail.com>
2022-03-07 19:35:14 -0300

    added basic ui

--- lib/views/index.dart
+++ lib/views/index.dart
@@ -9,14 +9,288 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
     @override
   Widget build(BuildContext context) {
+    /* double height = MediaQuery.of(context).size.height;
+    double width = MediaQuery.of(context).size.width;
+    print(width); */
+
     return Scaffold(
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          // mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'Worney app!',
-              // style: Theme.of(context).textTheme.headline4,
+            Container(
+              padding: const EdgeInsets.symmetric(vertical: 60),
+              child: const Text(
+                '00 : 00',
+                // style: Theme.of(context).textTheme.headline2,
+                style: TextStyle(
+                  color: Color(0xFF585666), //Color(0xFFFF9419)
+                  fontSize: 52,
+                  fontWeight: FontWeight.w600,
+                  fontFamily: 'Lexend',
+                ), 
+                textAlign: TextAlign.center,
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.symmetric(vertical: 6), //removed horizontal: 38 to make more responsive.
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    margin: const EdgeInsets.only(right: 2),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Text(
+                          'Entrada',
+                          style: TextStyle(
+                            color: Color(0xFF706E7A),
+                            fontSize: 13,
+                            fontWeight: FontWeight.w400,
+                            fontFamily: 'Inter',
+                          ),
+                        ),
+                        ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            primary: const Color(0xFF585666),
+                            fixedSize: const Size(147, 55),
+                            shape: const RoundedRectangleBorder(
+                              borderRadius: BorderRadius.only(
+                                bottomLeft: Radius.circular(5),
+                                topLeft: Radius.circular(5),
+                              ),
+                            ),
+                          ),
+                          onPressed: () => Null,
+                          child: const Text(
+                            '00 : 00',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 24,
+                              fontWeight: FontWeight.w600,
+                              fontFamily: 'Inter',
+                            ),  //
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.only(left: 2),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: [
+                        const Text(
+                          'Saída',
+                          style: TextStyle(
+                            color: Color(0xFF706E7A),
+                            fontSize: 13,
+                            fontWeight: FontWeight.w400,
+                            fontFamily: 'Inter',
+                          ),
+                        ),
+                        ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            primary: const Color(0xFF585666),
+                            fixedSize: const Size(147, 55),
+                            shape: const RoundedRectangleBorder(
+                              borderRadius: BorderRadius.only(
+                                bottomRight: Radius.circular(5),
+                                topRight: Radius.circular(5),
+                              ),
+                            ),
+                          ),
+                          onPressed: () => Null,
+                          child: const Text(
+                            '00 : 00',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 24,
+                              fontWeight: FontWeight.w600,
+                              fontFamily: 'Inter',
+                            ), //
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+
+            Container(
+              padding: const EdgeInsets.symmetric(vertical: 6), //removed horizontal: 38 to make more responsive.
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    margin: const EdgeInsets.only(right: 2),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Text(
+                          'Entrada',
+                          style: TextStyle(
+                            color: Color(0xFF706E7A),
+                            fontSize: 13,
+                            fontWeight: FontWeight.w400,
+                            fontFamily: 'Inter',
+                          ),
+                        ),
+                        ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            primary: const Color(0xFF585666),
+                            fixedSize: const Size(147, 55),
+                            shape: const RoundedRectangleBorder(
+                              borderRadius: BorderRadius.only(
+                                bottomLeft: Radius.circular(5),
+                                topLeft: Radius.circular(5),
+                              ),
+                            ),
+                          ),
+                          onPressed: () => Null,
+                          child: const Text(
+                            '00 : 00',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 24,
+                              fontWeight: FontWeight.w600,
+                              fontFamily: 'Inter',
+                            ),  //
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.only(left: 2),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: [
+                        const Text(
+                          'Saída',
+                          style: TextStyle(
+                            color: Color(0xFF706E7A),
+                            fontSize: 13,
+                            fontWeight: FontWeight.w400,
+                            fontFamily: 'Inter',
+                          ),
+                        ),
+                        ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            primary: const Color(0xFF585666),
+                            fixedSize: const Size(147, 55),
+                            shape: const RoundedRectangleBorder(
+                              borderRadius: BorderRadius.only(
+                                bottomRight: Radius.circular(5),
+                                topRight: Radius.circular(5),
+                              ),
+                            ),
+                          ),
+                          onPressed: () => Null,
+                          child: const Text(
+                            '00 : 00',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 24,
+                              fontWeight: FontWeight.w600,
+                              fontFamily: 'Inter',
+                            ), //
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+
+            Container(
+              padding: const EdgeInsets.symmetric(vertical: 6), //removed horizontal: 38 to make more responsive.
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    margin: const EdgeInsets.only(right: 2),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Text(
+                          'Entrada',
+                          style: TextStyle(
+                            color: Color(0xFF706E7A),
+                            fontSize: 13,
+                            fontWeight: FontWeight.w400,
+                            fontFamily: 'Inter',
+                          ),
+                        ),
+                        ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            primary: const Color(0xFF585666),
+                            fixedSize: const Size(147, 55),
+                            shape: const RoundedRectangleBorder(
+                              borderRadius: BorderRadius.only(
+                                bottomLeft: Radius.circular(5),
+                                topLeft: Radius.circular(5),
+                              ),
+                            ),
+                          ),
+                          onPressed: () => Null,
+                          child: const Text(
+                            '00 : 00',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 24,
+                              fontWeight: FontWeight.w600,
+                              fontFamily: 'Inter',
+                            ),  //
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.only(left: 2),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: [
+                        const Text(
+                          'Saída',
+                          style: TextStyle(
+                            color: Color(0xFF706E7A),
+                            fontSize: 13,
+                            fontWeight: FontWeight.w400,
+                            fontFamily: 'Inter',
+                          ),
+                        ),
+                        ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            primary: const Color(0xFF585666),
+                            fixedSize: const Size(147, 55),
+                            shape: const RoundedRectangleBorder(
+                              borderRadius: BorderRadius.only(
+                                bottomRight: Radius.circular(5),
+                                topRight: Radius.circular(5),
+                              ),
+                            ),
+                          ),
+                          onPressed: () => Null,
+                          child: const Text(
+                            '00 : 00',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 24,
+                              fontWeight: FontWeight.w600,
+                              fontFamily: 'Inter',
+                            ), //
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
             ),
           ],
         ),
